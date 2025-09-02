a6d69a2fd5b8f7ce19f79221216a436a0c60df84
peterhu04 <chun-pu.hu@mail.mcgill.ca>
2025-09-02 09:30:32 -0400

    Update splines.py

--- code alongs/splines.py
+++ code alongs/splines.py
@@ -1,1 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.interpolate
+plt.ion()
+
+def fun(x):
+  return 1/np.log(x)
+
+x=np.linspace(2,3,21) #21 points btwn 2,3
+dx=x[1]-x[0]
+y=fun(x)
 
